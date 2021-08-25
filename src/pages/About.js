41cbcebf8bd4b28f94cc43e002eae3332f9080bb
celeis <gonca.negis@gmail.com>
2021-08-25 15:21:33 +0300

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit non
+        inventore expedita cupiditate commodi a totam voluptatum doloribus
+        reprehenderit repellat id libero, rerum beatae. Maiores expedita
+        molestiae, non eum commodi consequatur nihil odio unde neque aliquam.
+        Dolores labore aut quae.
+      </p>
+    </section>
+  )
+}
+
+export default About
